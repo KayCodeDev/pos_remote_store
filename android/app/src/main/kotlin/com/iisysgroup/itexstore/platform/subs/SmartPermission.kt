0f2bee9f5e5de1a6a536c1986b244f394aa1ef1d
Colken Developer <kenny@colkenconsult.com>
2024-07-12 10:46:36 +0100

    update

--- android/app/src/main/kotlin/com/iisysgroup/itexstore/platform/subs/SmartPermission.kt
+++ android/app/src/main/kotlin/com/iisysgroup/itexstore/platform/subs/SmartPermission.kt
@@ -13,7 +13,6 @@ class SmartPermission(private val context: Context) {
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.RECEIVE_BOOT_COMPLETED,
         Manifest.permission.READ_PHONE_STATE,
-//        Manifest.permission.READ_PHONE_NUMBERS,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.READ_EXTERNAL_STORAGE,
         "android.permission.ACCESS_NETWORK_STATE",

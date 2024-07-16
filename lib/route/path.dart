02f29ca483a51495d43d641b24decfebb2c1ae02
Colken Developer <kenny@colkenconsult.com>
2024-07-16 12:04:47 +0100

    update

--- lib/route/path.dart
+++
@@ -1,5 +0,0 @@
-class RouterPaths {
-  static const String init = '/init';
-  static const String store = '/store';
-  static const String storeApp = '/store/app';
-}
--- lib/utils/enums.dart
+++
@@ -1,1 +0,0 @@
-enum AppAction { install, update, open }
